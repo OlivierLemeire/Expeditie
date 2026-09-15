f885f90c2d879fc56af94061ddb54b6836f30d5e
OlivierLemeire <olivierlemeire@gmail.com>
2026-09-15 10:41:18 +0200

    Fix indentation for spinner context manager

--- streamlit_app.py
+++ streamlit_app.py
@@ -347,7 +347,7 @@ Hier zijn de analyses van de leerlingen:
 {analyses}
 """
 
-  with st.spinner("Jullie analyse wordt nagekeken..."):
+    with st.spinner("Jullie analyse wordt nagekeken..."):
 
     try:
         response = client.models.generate_content(

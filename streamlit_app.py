b068f38a384abde5582eb0b718a0dc8db50bbdbf
OlivierLemeire <olivierlemeire@gmail.com>
2026-09-15 09:18:56 +0200

    Update app title and add personality sliders

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🏝️ Expeditie Eiland")
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Jullie zijn zes maanden gestrand op een onbewoond eiland. "
+    "Kunnen jullie een groep samenstellen die goed blijft functioneren?"
 )
+
+st.subheader("Persoon 1")
+
+naam = st.text_input("Naam")
+
+openheid = st.slider("Openheid", 1, 10, 5)
+consci = st.slider("Consciëntieusheid", 1, 10, 5)
+extraversie = st.slider("Extraversie", 1, 10, 5)
+altruisme = st.slider("Altruïsme", 1, 10, 5)
+neuroticisme = st.slider("Neuroticisme", 1, 10, 5)
+
+if st.button("Toon persoonlijkheid"):
+    st.write(f"### {naam}")
+    st.write(f"Openheid: {openheid}/10")
+    st.write(f"Consciëntieusheid: {consci}/10")
+    st.write(f"Extraversie: {extraversie}/10")
+    st.write(f"Altruïsme: {altruisme}/10")
+    st.write(f"Neuroticisme: {neuroticisme}/10")
